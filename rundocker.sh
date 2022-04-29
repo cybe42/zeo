4626f5f39b02eb0596dad1a87156f6e713ef525e
cybe42 <usercybe@gmail.com>
2022-04-29 21:43:30 +0100

    root check for docker scripts

--- rundocker.sh
+++ rundocker.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-sudo docker run -it cybe42/zeo
+ROOT_UID=0
+E_NOTROOT=87
+
+
+if [ "${UID:-$(id -u)}" -ne "$ROOT_UID" ]; then
+    echo 'Error: Please run this script with root priviliges.'
+    exit $E_NOTROOT
+fi
+docker run -it cybe42/zeo
